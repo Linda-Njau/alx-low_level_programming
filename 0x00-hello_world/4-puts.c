cc76f534a0e5df73d6fa6bad286ad1497bff51ab
Linda-Njau <lindanjau21@gmail.com>
2022-09-09 03:38:53 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,19 @@
+/*
+ * file: 4-puts.c
+ * Author: Linda Njau
+ */
+
 #include <stdio.h>
 
 /**
- * main- print phrase "programming is like building a multilingual puzzle
- * return: 0 if exited properly
+ * main- print phrase "programming is like building a multilingual puzzle, followed by new line.
+ *
+ * return: Always 0
  */
 int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
+
 	return (0);
+
 }
