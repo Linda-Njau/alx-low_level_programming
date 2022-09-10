f258b5df40785a28e6adac0de5ccf10c522f466c
Linda-Njau <lindanjau21@gmail.com>
2022-09-09 17:51:07 -0700

    putchar

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prinf number
+ *
+ * Description: using the main function
+ * this program prints number
+ * Return: 0
+ */
+int main(void)
+{  
+char c;
+for (c = 'z'; c >= 'a'; c--)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
+
