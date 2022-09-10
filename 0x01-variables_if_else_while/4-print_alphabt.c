d92480b83c0877856e48a648e13acc1480bfeece
Linda-Njau <lindanjau21@gmail.com>
2022-09-09 17:30:28 -0700

    alph

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print alphabet
+ *
+ * Description: using the main function
+ * this program prints alphabet
+ * Return: 0
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+if (ch != 'e' && ch != 'q')
+{
+	putchar(ch);
+}
+}
+putchar('\n');
+return (0);
+}
