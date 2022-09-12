8d9971fe39440133fe17c6cc66807a424c15db87
Linda-Njau <lindanjau21@gmail.com>
2022-09-12 15:50:22 -0700

    putchar

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
- * Main - print numbers
- *
+ * Main -main function print numbers
  * Description: printing numbers using putchar
  *
  *Return: return 0
@@ -9,7 +8,7 @@
 int main(void)
 {
 int i;
-for (i=48; i <58; i++)
+for (i = 48; i < 58; i++)
 {
 putchar(i);
 }
