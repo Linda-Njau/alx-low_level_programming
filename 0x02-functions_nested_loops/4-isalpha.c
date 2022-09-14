ebce1588406265ac7bb9d04b4adf10673926f01b
Linda-Njau <lindanjau21@gmail.com>
2022-09-14 15:55:37 -0700

    alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
  * _isalpha - shows if character is letter 
- * 
+ *@c: is the int that will use as argument
+
  * Return: one if letter is uppercase, zero otherwise.
  */
 int _isalpha(int c)
 {
-if ((c >= 'A' && C <= 'Z') || (c >= 'a' && c <= 'z'))
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 {
 return(1);
 }
