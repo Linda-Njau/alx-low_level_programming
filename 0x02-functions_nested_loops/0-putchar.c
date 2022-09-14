eda073457c42ecdc0ddc47170e369fbc77cb19f2
Linda-Njau <lindanjau21@gmail.com>
2022-09-14 06:52:09 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,9 +8,13 @@
 int main(void)
 {
 char ch;
-ch =  _putchar;
-putchar(ch);
-putchar('\n');
-
+_putchar('_')
+_putchar('p')
+_putchar('u')
+_putchar('t')
+_putchar('c')
+_putchar('h')
+_putchar('a')
+_putchar('r')
 return(0);
 }
