12aa238507169af3a7b1c89350a57fa0258b5358
Linda-Njau <lindanjau21@gmail.com>
2022-09-14 14:40:03 -0700

    alphax10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet ten times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+char alphabet
+int i
+for (i = 1; i <= 10; i++)
+{
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+putchar(alphabet);
+}
+putchar('\n');
+}
+}
