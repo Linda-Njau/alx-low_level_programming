79d8bac2d3d55152ae93caffafddd2f22d4a8d8f
Linda-Njau <lindanjau21@gmail.com>
2022-09-14 15:35:35 -0700

    alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha - shows if letter is uppercase
+ *
+ * Return: one if letter is uppercase, zero otherwise.
+ */
+int _isalpha(int c)
+{
+if ((c >= 'A'; && C <= 'Z') || (c >= 'a'; && c <= 'z'))
+{
+return(1);
+}
+else
+return(0);
+}
