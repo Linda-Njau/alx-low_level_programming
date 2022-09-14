38bbd3945b7e0c990cb6440c24931f08394f181a
Linda-Njau <lindanjau21@gmail.com>
2022-09-14 07:47:31 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "1-alphabet.c"
+/**
+ * main - print alphabet
+ * Description: using _putchar twice
+ *
+ * return: 0
+ */
+void print_alphabet(void);
+{
+char c;
+for (c = 'a'; c =< z; c++)
+{
+_putchar(ch);
+_putchar('\n');
+
+return (0);
+}
