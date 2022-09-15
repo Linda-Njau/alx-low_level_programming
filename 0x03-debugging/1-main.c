a37c5b0acf6c5cbb1b3e97ad488f7379cde5c86c
Linda-Njau <lindanjau21@gmail.com>
2022-09-15 12:19:25 -0700

    error

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,17 +7,17 @@
 
 int main(void)
 {
-	/* int i; */
+	int i;
 
 	printf("Infinte loop incoming :(\n");
 
-	/* i = 0; */
-
-	/* while (i < 10) */
-	{
-	/* putchar(i); */
-	}
-	
+	 i = 0;
+	 /*
+	  * while (i < 10)
+	  * {
+	  * putchar(i);
+	  * }
+	  */
 	  printf("Infinite loop avoided! \\o/\n");
 
 	  return (0);
