022f2debec2c91d04e039b1768f807488f6a23d3
Linda-Njau <lindanjau21@gmail.com>
2022-09-19 15:53:41 -0700

    header filr

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,13 +1,20 @@
-#include <unistd.h>
-/**
-  * _putchar - writes the character c to stdout
-  * @c: The character to print
-  * Return: On success 1.
-  * On error, -1 is returned, and errno is set appropriately
-  */
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+/**
+ * void prototypes()
+ * int prototypes()
+ */
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n)i;
+char *_strcpy(char *dest, char *src);
+
+#endif /*MAIN_H*/
 
