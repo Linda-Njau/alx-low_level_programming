f03e1e5b728b9d9a47505bbb8de7dbe385114e4e
Linda-Njau <lindanjau21@gmail.com>
2022-09-19 17:59:00 -0700

    rev string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,9 +10,9 @@ void rev_string(char *s)
 {
 	int i, tmp, len = _strlen(s);
 
-	for (i = 0; i , len / 2; i++)
+	for (i = 0; i < len / 2; i++)
 	{
-	tmp = (s + i);
+	tmp = *(s + i);
 	*(s + i) = *(s + len - i - 1);
 	*(s + len - i -1) = tmp;
 	}
