cec88c0cd4f3fcf186f20cc6ab50ce1269bf359e
Linda-Njau <lindanjau21@gmail.com>
2022-09-22 08:00:18 -0700

    first commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcat - appends string overwtiting the terminating null 
+ * @dest: one string
+ *@strcat: a second string
+ *return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	char dest[len] = 0;
+	char src[i] = 0;
+
+	for (; i != '\0'; i++)
+	dest[len] + i = src[i]
+	dest[len] + i = '\0'
+
+return (dest);
+}
