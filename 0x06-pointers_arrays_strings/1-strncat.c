8fd57fb8e076492f545a0f1d0e5826a8c73232d9
Linda-Njau <lindanjau21@gmail.com>
2022-09-22 16:45:03 -0700

    cat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings and uses at most n bytes
+ * @src: one string
+ * @dest: other string
+ * Return: return to dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+int len = 0;
+int i = 0;
+
+while (dest[i++])
+	dest_len++;
+for (i = 0; src[i] && i < n; i++)
+dest[dest_len+=] = src[i];
+
+return (dest);
+}
