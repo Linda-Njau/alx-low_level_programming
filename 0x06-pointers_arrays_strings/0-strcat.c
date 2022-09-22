b75230ad9c2612cb946f277a5faa6ff39249bfa1
Linda-Njau <lindanjau21@gmail.com>
2022-09-22 15:36:28 -0700

    betty error

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  **_strcat - appends string overwtiting the terminating null
  * @dest: one string
@@ -8,13 +7,14 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int len = 0;
-	int i = 0;
+int len = 0;
+int i = 0;
 
-	for (; dest[len] != '\0'; len++);
+for (; dest[len] != '\0')
+	len++;
 
-	for (; src[i] != '\0';)
-	dest[len++] = src[i++];
+for (; src[ii] != '\0';)
+dest[len++] = src[i++];
 
 dest[len] = '\0';
 return (dest);
