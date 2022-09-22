c1ca8e279a3147109f5cd7a225d807832ff910c8
Linda-Njau <lindanjau21@gmail.com>
2022-09-22 08:38:46 -0700

    cat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,10 +11,10 @@ char *_strcat(char *dest, char *src)
 	int len = 0;
 	int i = 0;
 
-	for (; len != '\0'; len++)
-	for (; i != '\0'; i++)
-	dest[len] + i = src[i];
-	dest[len] + i = '\0';
+	for (; dest[len] != '\0'; len++)
+	for (; src[i] != '\0'; i++)
+	dest[len + i] = src[i];
+	dest[len + i] = '\0';
 
 return (dest);
 }
