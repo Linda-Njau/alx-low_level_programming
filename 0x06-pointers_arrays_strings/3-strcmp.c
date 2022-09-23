dd760c9a26432e52e0e847a69f015e04b9c0d93f
Linda-Njau <lindanjau21@gmail.com>
2022-09-22 17:23:49 -0700

    betty error

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
- * _strcmp - Function which compare two strings 
+ *_strcmp - Function which compare two strings 
  * @s1
  * @s2
  * Return: zero if identical
- * 	   negative if s1 < s2
- * 	   positive if s1 > s2
+ * negative if s1 < s2
+ * positive if s1 > s2
  */
- int _strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
+
 	while (s1[i] == s2[i])
 	{
 	if (s1[i] == '\0')
