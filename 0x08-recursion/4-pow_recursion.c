0907a538fe8095c748d981547e53963b35490dcf
Linda-Njau <lindanjau21@gmail.com>
2022-09-28 17:42:04 -0700

    power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _pow_recursion(int x, int y);
+ * @y: one value
+ * @x: second value
+ * Return: value of x power y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+	return (-1);
+	}
+	if (y == 0)
+	{
+	return (1);
+	}
+	else
+	{
+	return
+	(x * _pow_recursion(x, y - 1));
+	}
+}
+	
+
