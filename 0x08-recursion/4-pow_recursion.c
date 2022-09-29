180b9f9a3dcd6c580ca67a9a0367f59b43c14a87
Linda-Njau <lindanjau21@gmail.com>
2022-09-28 17:46:56 -0700

    power betty

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _pow_recursion(int x, int y);
+ * _pow_recursion - find power
  * @y: one value
  * @x: second value
  * Return: value of x power y
@@ -22,5 +22,4 @@ int _pow_recursion(int x, int y)
 	(x * _pow_recursion(x, y - 1));
 	}
 }
-	
 
