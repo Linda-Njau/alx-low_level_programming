89da4c8a892fe4011cd75c555ec529bec321ddfa
Linda-Njau <lindanjau21@gmail.com>
2022-09-30 06:44:18 -0700

    print name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: string array vector
+ * Retrun: nothing
+ */
+
+int main(int  argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+
+return (0);
+}
