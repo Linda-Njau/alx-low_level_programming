7cb252131849fea171f1f5da271ccac94dcbdd6f
Linda-Njau <lindanjau21@gmail.com>
2022-10-13 08:56:44 -0700

    sum

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,12 @@
+#ifndef SUM_H
+#define SUM_H
+/**
+ * filename - 4-sum.h
+ * Description: header file that defines SUM(x,y) that 
+ * expands to sum of x and y
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
+
