ac8c345299a86f25386348a46258970589260d36
Linda-Njau <lindanjau21@gmail.com>
2022-10-13 01:40:56 -0700

    first commit

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+/**
+ * file: 0-object_like macro.h
+ * Desc: Header file defining macro SIZE
+ * to expansion of 1024
+ *
+#define SIZE 1024
+
+#endif
+
