e4bbc3338b6ef2d2a561d6e092ead898799fd683
Linda-Njau <lindanjau21@gmail.com>
2022-10-13 08:48:17 -0700

    ABS(x)

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+ * File name - 3-function_like_macro.h
+ * Description: Header file for macro ABS(X) 
+ * whsoe expansion comuptes absolute value of x
+ */
+
+#define ABS(x) (((x) < (0)) ? ((x) * (-1)) : (x))
+
+#endif
