a27739fe4e842e555380c2f9007f074417f7018f
Linda-Njau <lindanjau21@gmail.com>
2022-10-25 14:49:09 -0700

    error fixed

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,17 +4,21 @@
  * @h: pointer to first node
  * Return: returns number of nodes
  */
-size_t print_list(const list_t *h);
+size_t print_list(const list_t *h)
 {
        size_t count = 0;
-       const list_t nodeptr = NULL;
+       const list_t *nodeptr = NULL;
 	
 	nodeptr = malloc(sizeof(list_t));
+
+	if (nodeptr == NULL)
+		return (0);
+
 	nodeptr = h;
 	while (nodeptr != NULL)
 	{
 		if (nodeptr->str == NULL)
-			printf("[0] (nil)\n")
+			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", nodeptr->len nodeptr->str);
 		nodeptr = nodeptr->next;
