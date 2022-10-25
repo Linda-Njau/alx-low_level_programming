810ea4d0577f8343028ac58d420030af8ec736f0
Linda-Njau <lindanjau21@gmail.com>
2022-10-25 16:43:51 -0700

    new tasks

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * first_func - prints a sentence before the main
+ * Return: return nothing
+ */
+void first_func(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
