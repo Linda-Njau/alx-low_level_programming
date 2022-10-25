292ea7dbc6c2506a51d90c3b6445a9e382349e60
Linda-Njau <lindanjau21@gmail.com>
2022-10-25 15:20:13 -0700

    element count

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * list_len - returns number elements in a linked list
+ * @h: pointer to first node
+ * Return: number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+	const list_t *nodeptr = NULL;
+
+	nodeptr = malloc(sizeof(list_t));
+
+	nodeptr = h;
+	while (nodeptr != NULL)
+	{
+		nodeptr->next;
+		count++;
+	}
+	return (count);
+}
