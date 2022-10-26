220703807f7be8a554cecbc567d2681238a8ef46
Linda-Njau <lindanjau21@gmail.com>
2022-10-25 17:00:22 -0700

    More errors

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,13 +8,13 @@
 
 void free_list(list_t *head)
 {
-	list_t *temp;
+	list_t *new;
 
 	while (head != NULL)
 	{
-		temp = head->next;
+		new = head->next;
 		free(head->str);
 		free(head);
-		head = temp;
+		head = new;
 	}
 }
