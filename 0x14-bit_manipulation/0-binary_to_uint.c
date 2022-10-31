a70076f203881627b9b90712b4640202dcdd7c30
Linda-Njau <lindanjau21@gmail.com>
2022-10-31 14:41:11 -0700

    fixed betty error

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,9 +2,10 @@
 #include <string.h>
 
 /**
- * binary_to_unit - converts binary number to unsigned int
+ * binary_to_uint - converts binary number to unsigned int
  * @b: string containing binary number
- * Return: converted number or 0 if there is one or more chars not 0 or 1, or b is NULL
+ * Return: converted number or 0 if there is one or
+ * *more chars not 0 or 1, or b is NULL
  */
 unsigned int binary_to_uint(const char *b)
 {
