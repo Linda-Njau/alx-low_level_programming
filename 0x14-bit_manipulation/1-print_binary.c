629639953c7a986d935f9d251ef12d43647e3a93
Linda-Njau <lindanjau21@gmail.com>
2022-11-01 02:54:48 -0700

    print binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**print_binary - prints binary number
+ * @n: number being converted to binary
+ * Return: nothing
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int mask = 10000000;
+        	
+	while (mask != '\0')
+	{
+		if (mask & n)
+			_putchar('1');
+		else
+			_putchar('0');
+		mask >>= 1;
+	}
+	return;
+}
+
+	
