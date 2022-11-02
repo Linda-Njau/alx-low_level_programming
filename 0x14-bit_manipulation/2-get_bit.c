74c55de7361793d6d6de7a6db8dbadc630a06339
Linda-Njau <lindanjau21@gmail.com>
2022-11-02 11:01:51 -0700

    fixed betty error

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,13 +8,14 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int mask = 1 << index;
+
 	if (n == 0)
 		return (-1);
 	if (index >= 64)
 		return (-1);
-	if (( n & mask) == 0)
+	if ((n & mask) == 0)
 		return (0);
 	else
-		return(1);
+		return (1);
 }
 
