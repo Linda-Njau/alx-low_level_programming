675855854cdce25fae1186846a95aa07ba143af6
Linda-Njau <lindanjau21@gmail.com>
2022-11-02 10:09:54 -0700

    betty error fixed

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,11 +1,12 @@
 #include "main.h"
-/**print_binary - prints binary number
+/**
+ * print_binary - prints binary number
  * @n: number being converted to binary
  * Return: nothing
  */
 void print_binary(unsigned long int n)
 {
-	if (n>>1)
+	if (n >> 1)
 		print_binary(n >> 1);
 	_putchar((n & 1) + '0');
 }
