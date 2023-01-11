cde56d6f2d10f1f8541dba8e65645babd0b2e7cc
Linda-Njau <lindanjau21@gmail.com>
2023-01-11 10:20:01 -0800

    error

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,7 +9,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *tmp;
 
-	if (key == NULL || ht == NULL)
+	if (key == NULL || ht == NULL || *key == '\0')
 		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
