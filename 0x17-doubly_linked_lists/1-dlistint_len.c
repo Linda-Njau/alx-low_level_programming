5a205006a85dc4d6121d5f677f26ea8c6927d238
Linda-Njau <lindanjau21@gmail.com>
2023-01-24 07:33:56 -0800

    no of elements

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * dlistint_len - return number of elements
+ * @h: header of list
+ * Return: number of elements
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t counter;
+
+	while(h != NULL)
+	{
+		counter++;
+		h = h->next;
+	}
+	return (counter);
+}
