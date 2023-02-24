d99e720bb4c6d1bf729f6aef2dd03020e6871abf
Linda-Njau <lindanjau21@gmail.com>
2023-02-24 05:16:12 -0800

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -10,7 +10,7 @@ def island_perimeter(grid):
         for j in range(len(grid[i])):
             if grid[i][j]  == 1:
                 size += 1
-            if grid[i][j] == 1 and grid[i][j+1] == 1 and grid[i][j-1] == 0:
+            if grid[i][j] == 1 and grid[i-1][j] == 1 and grid[i][j+1] == 0:
                 size += 1
         perimeter= size * 2
     return perimeter 
