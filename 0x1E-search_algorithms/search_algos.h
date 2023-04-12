7c3a1a7350ff00ac4e673a46056d92ebb6986223
Linda-Njau <lindanjau21@.com>
2023-04-12 05:37:32 -0700

    0-linear.c

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,10 +2,11 @@
 #define SEARCH_ALGOS_H
 #include <stdio.h>
 #include <stddef.h>
+
 /**
-* header file
-*  defines prototypes
-*/
+ *header file
+ * defines prototypes
+ */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
