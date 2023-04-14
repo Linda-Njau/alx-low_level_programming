28aedde64a77eefb447f1a5d152e225c526a8726
Linda-Njau <lindanjau21@.com>
2023-04-14 09:06:58 -0700

    104-advanced_binary.c

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,41 +1,33 @@
 #include "search_algos.h"
 /**
- * advanced_binary - improvement on binary search to return first occurrence of value
+ * advanced_binary - modifies binary search to find first occurence of value
  * @array: pointer to array
- * @size: size of array
- * @value: value of array
- * Return: index where the value is located or -1 if value is not found
+ * @size: size of arrays
+ * @value: value to be found
+ * Return: index of firsat occurence or -1 on failure
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-    size_t half, i;
-    int subarray_idx;
-    int *original_array;
+	size_t i, half;
+	int index;
 
-    if (array == NULL || size == 0)
-        return (-1);
-
-    printf("Searching in array: ");
-    for (i = 0; i < size; i++)
-        printf("%d%s", array[i], i == size - 1 ? "\n" : ", ");
-
-    original_array = array;
-    half = (size - 1) / 2;
-    subarray_idx = array - original_array;
-
-    if (array[half] == value)
-    {
-        if (half == 0 || array[half - 1] < value)
-            return (subarray_idx + half);
-        else
-            return (advanced_binary(array, half + 1, value));
-    }
-
-    if (array[half] > value)
-        return (advanced_binary(array, half, value));
-
-    subarray_idx += half + 1;
-    half = (size - half - 1) / 2;
-    return (advanced_binary(array + half, size - half, value) + subarray_idx);
+	if (array == NULL || size == 0)
+		return (-1);
+	printf("Searching in array: ");
+	for (i = 0; i < size; i++)
+		printf("%d%s", array[i], i == size - 1 ? "\n" : ", ");
+	half = (size - 1) / 2;
+	if (array[half] == value)
+	{
+		if (half == 0 || array[half - 1] < value)
+			return ((int)half);
+	}
+	if (array[half] < value)
+	{
+		index = advanced_binary(array + half + 1, size - half - 1, value);
+		if (index == -1)
+			return (-1);
+		return ((int)half + index + 1);
+	}
+	return (advanced_binary(array, half + 1, value));
 }
-
